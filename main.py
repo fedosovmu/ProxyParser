1fcd73a468f09a830bc92470c44e5537b385a7a4
Максим Федосов <mocoronco@gmail.com>
2020-12-06 19:58:27 +0000

    Добавил функцию сохранения страницы в файл что бы не посылать запрос на сервер

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
+from replit import db
 
 
 def get_free_proxy_url():
     try:
         get_proxy_url = 'https://api.getproxylist.com/proxy'
         proxy_info = requests.get(get_proxy_url).json()
+        protocol = proxy_info['protocol']
+        if protocol != 'http' and protocol != 'https':
+            print('Неподходящий протокол проксисервера')
+            return None
         proxy_url = '{}://{}:{}'.format(proxy_info['protocol'], proxy_info['ip'], proxy_info['port'])
         print('Ипользуемый прокси: ', proxy_url)
-        return proxy_url
+        return { proxy_info['protocol']: proxy_url }
     except:
         print('Проксисервер недоступен')
         return None
@@ -29,15 +34,27 @@ def get_page():
     user_agent = {'User-agent': get_random_user_agent()}
     proxy = get_free_proxy_url()
     page = requests.get(url, headers = user_agent, proxies=proxy)
-    return page
+    return page.text
 
 def parse_page(page):
-    soup = BeautifulSoup(page.text, 'lxml')
+    #soup = BeautifulSoup(page.text, 'lxml')
+    soup = BeautifulSoup(page, 'lxml')
     print(soup.prettify())
+    print('kek')
+
+def save_page_to_txt_file(page):
+    file = open("db/hiddenme_page.txt","a") 
+    file.writelines(page)
+    file.close()
+    print('file saving')
 
 
-page = get_page()
+#page = get_page() 
+#db["hidemy_site_page"] = page
+page = db["hidemy_site_page"]
 parse_page(page)
+
+print('end')
 
 
 
